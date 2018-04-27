9c028034c17551c9b18911b65ed6bffc688fa9b4
anshulsacheti <anshul.sacheti@gmail.com>
2018-04-26 22:24:43 -0400

    Data Challenge 2 final code

--- Data_Challenge_2/README.txt
+++ Data_Challenge_2/README.txt
@@ -1,16 +1,3 @@
-How to run: python3 main.py
-
-Final fairness score: 0.069336
-Predicted nodes file: final_predicted_nodes.csv
-
-Particulars of solution:
-
-I made a subgraph of all recommended edges. Thus if an edge is recommended multiple times, that
-recommendation count is treated as 1. For an edge (u,v) since "v is recommended for u", I counted that
-as an increment for node v's recommendation count, but nothing for node u. If there was a match in score,
-then a node was randomly chosen and if a node had no neighbors then it's prediction was randomly chosen
-from all nodes.
-
 Dataset: Stack Overflow Temporal Network
 https://snap.stanford.edu/data/sx-stackoverflow.html
 
@@ -20,3 +7,83 @@ The interaction represented by a directed edge (u, v, t):
 user u answered user v's question at time t (in the graph sx-stackoverflow-a2q)
 These graphs were constructed from the Stack Exchange Data Dump.
 Node ID numbers correspond to the 'OwnerUserId' tag in that data dump.
+
+The timestamps were categorized to above or below 1380000000 (chosen by finding a value
+that most evenly split the dataset in 2) represented with an attribute "A"/"B" roughly giving
+a 52%/48% split in total data. I included my code to do so.
+
+Due to the size of the dataset, it was reduced to 400000 edges for reduced runtime (on the order of
+an hour per iteration). This reduced and converted dataset is included with the submission. The edges
+and attributes are separated into 2 files, similar to the first part of this Data Challenge.
+
+Subset node count: 82436
+Subset edge count: 400000
+
+For testing:
+I randomly take 100000 edges and remove them from the input dataset and compare after running how
+my new graph of recommended edges compare in both accuracy and fairness. This is the case for testing
+both the Instagram and Stackoverflow datasets.
+
+How to run:
+
+For Instagram Dataset:
+Adamic Adar: python3 main.py --test --iters 30
+Custom Algo: python3 main.py --test --iters 30 --SO
+
+For Stackoverflow Dataset:
+Adamic Adar: python3 main.py --test --iters 30 --SO
+Custom Algo: python3 main.py --customAlgo --test --iters 30 --SO
+
+Particulars of solution:
+
+Adamic Adar:
+I made a subgraph of all recommended edges. Thus if an edge is recommended multiple times, that
+recommendation count is treated as 1. For an edge (u,v) since "v is recommended for u", I counted that
+as an increment for node v's recommendation count, but nothing for node u. If there was a match in score,
+then a node was randomly chosen and if a node had no neighbors then it's prediction was randomly chosen
+from all nodes.
+
+Custom Algo:
+I built on the idea of common neighbors providing information about recommended edges. The algorithm
+was similar to the skeleton provided by Adamic Adar. You generate a score based on neighbors and then
+assign every relevant node a score, choosing the highest score as recommendation.
+
+The algorithm first gets a list of mutual neighbors between node u and node v. For each of those mutual
+neighbor nodes (let's call it the set X), you take x's neighbors. And for each of x's neighbors, increment
+by 1 if x's neighbor is in the set of all nodes that could share a neighbor with u. Lastly, to encourage nodes
+with low neighbor counts, we use a 1/np.log(sum) to strengthen nodes with fewer shared neighbors. This follows
+a method very similar to Adamic Adar but is more akin to how strong a node v is as part of a clique with node u.
+
+Similar to Adamic Adar, I made a subgraph of all recommended edges. Thus if an edge is recommended
+multiple times, that recommendation count is treated as 1. For an edge (u,v) since "v is recommended
+for u", I counted that as an increment for node v's recommendation count, but nothing for node u.
+If there was a match in score, then a node was randomly chosen and if a node had no neighbors then
+it's prediction was randomly chosen from all nodes.
+
+Instagram Dataset (30 iters)
+Custom:
+Average Fairness: 0.066574
+Average Percent correct: 0.000101
+
+AA:
+Average Fairness: 0.070224
+Average Percent correct: 0.000094
+
+Stackoverflow Dataset (30 iters)
+Custom:
+Average Fairness: 0.235560
+Average Percent correct: 0.001138
+
+AA:
+Average Fairness: 0.234870
+Average Percent correct: 0.001131
+
+Results:
+
+The new algorithm beats Adamic Adar on both average fairness and average percent correct on the Instagram
+data, while only doing slightly better in average correctness on the stackoverflow data. The disparity
+could be due to a variety of factors but it could be due to how Instagram is inherently groups of friends
+interacting and thus cliques are more likely to be formed. This could skew the results as there are many nodes
+that could meet the criteria to be recommended. Stackoverflow is primarily meant as Q/A for programmers and
+thus having groups of friends interact there is unlikely. This means that edges that form are more unique
+and are more likely to indicate a unique relationship and accordingly, strength in that relationship.
--- Data_Challenge_2/main.py
+++ Data_Challenge_2/main.py
@@ -9,7 +9,7 @@ import datetime
 import random
 
 # Read in graph
-def generateGraph():
+def generateInstagramGraph():
     """
     Generate edge graph via networkx and edge file
 
@@ -34,13 +34,40 @@ def generateGraph():
 
     return G
 
-def runAdamicAdar(G, predictNodes):
+# Read in graph
+def generateStackOverflowGraph():
     """
-    Run Adamic adar algorithm and generate edge recommendations for nodes in file
+    Generate edge graph via networkx and edge file
+
+    Input:
+        None
+
+    Output:
+        Networkx graph with edges + node genders
+
+    Returns
+        G: nx edge graph
+    """
+
+    G = nx.read_edgelist(path="nodeTimestampEdges_subset.csv", nodetype=int, delimiter=',')
+
+    with open("nodeTimestampCategory.csv", "r") as file:
+        for i,line in enumerate(file.readlines()):
+            data = line.split("\n")
+            node, relativeTimestamp = data[0].split(",")
+            G.add_node(int(node))
+            nx.set_node_attributes(G, name="relativeTimestamp", values={int(node):relativeTimestamp})
+
+    return G
+
+def runCustomAlgo(G, predictNodes, dataset):
+    """
+    Run Custom algorithm and generate edge recommendations for nodes in file
 
     Input:
         G - networkx graph
         predictNodes - list of nodes to predict
+        dataset - string indicating what dataset is being run
 
     Output:
         nodeRecPair - dict of node recommendations
@@ -58,7 +85,111 @@ def runAdamicAdar(G, predictNodes):
 
     nodeRecPair = dict(zip(predictNodes, [0]*len(predictNodes)))
 
-    # pool = ThreadPool(16)
+    # Run custom algo on all nodes
+    nodesCompleted = 0
+    for i,node1 in enumerate(nodeRecPair):
+
+        # Untouched nodes
+        if nodeRecPair[node1]==0:
+
+            #Only nodes within a distance 2 of base node can have neighbors
+            possibleNeighbors = list(nx.ego_graph(G,node1,radius=2).nodes())
+            possibleNeighborSet = set(possibleNeighbors)
+
+            node1_neighbors = list(G.neighbors(node1))
+            neighborsDict[node1] = node1_neighbors
+            scoreDict[node1] = {}
+
+            if nodesCompleted % 2000 == 0:
+                print("Finished %d nodes, %s" % (nodesCompleted, datetime.datetime.time(datetime.datetime.now())))
+            nodesCompleted += 1
+
+            # Calculate score per node
+            for node2 in possibleNeighbors:
+
+                # Ego graph returns list of nodes including input node and it's neighbors
+                if node2 == node1 or node2 in node1_neighbors:
+                    continue
+                else:
+                    if node2 in neighborsDict:
+                        node2_neighbors = neighborsDict[node2]
+                    else:
+                        node2_neighbors = list(G.neighbors(node2))
+                        neighborsDict[node2] = node2_neighbors
+
+                node1_neighbors = set(node1_neighbors)
+                node2_neighbors = set(node2_neighbors)
+                intersect = node1_neighbors.intersection(node2_neighbors)
+
+                # Calculate score
+                if intersect:
+                    score = 0
+                    # Update score against all neighbors
+                    for n1 in intersect:
+                        if n1 in neighborsDict:
+                            neighbors = neighborsDict[n1]
+                        else:
+                            neighbors = list(G.neighbors(n1))
+                            neighborsDict[n1] = neighbors
+                        total = 0
+                        for n2 in neighbors:
+                            if n2 in possibleNeighborSet:
+                                total += 1
+                        if total>1:
+                            score += 1/np.log(total)
+                    scoreDict[node1][node2] = score
+
+            # Choose best score for edge recommendation
+            # Add new node/edge to graph with props
+            try:
+                bestNode = max(scoreDict[node1], key=scoreDict[node1].get)
+            except ValueError:
+                bestNode = np.random.choice(graphNodes)
+                while bestNode==node1 or bestNode in node1_neighbors:
+                    bestNode = np.random.choice(graphNodes)
+            nodeRecPair[node1] = (node1, bestNode)
+
+            # Testing
+            if 0==1 and len(scoreDict[node1].keys())<6 and len(node1_neighbors)<6:
+                print("Base Node: %d, NodeRec: %d" % (node1, bestNode))
+                print("ScoreDict: %s" % scoreDict[node1])
+                print("Neighbors: %s" % node1_neighbors)
+                print("-----------\n")
+            # predictions.extend([(node1, bestNode)])
+
+    # Write all recommendations out
+    with open("predicted_nodes_custom_"+dataset+".csv", "w") as predicted:
+        for p in predictNodes:
+            predicted.write("{}\n".format(nodeRecPair[p]))
+
+    return [nodeRecPair, scoreDict]
+
+
+def runAdamicAdar(G, predictNodes, dataset):
+    """
+    Run Adamic adar algorithm and generate edge recommendations for nodes in file
+
+    Input:
+        G - networkx graph
+        predictNodes - list of nodes to predict
+        dataset - string indicating what dataset is being run
+
+    Output:
+        nodeRecPair - dict of node recommendations
+        scoreDict - dict of node rec scores
+
+    Returns:
+        list of dicts, [nodeRecPair, scoreDict]
+    """
+
+    graphNodes = list(G.nodes)
+
+    neighborsDict = {}
+    scoreDict = {}
+    predictions = []
+
+    nodeRecPair = dict(zip(predictNodes, [0]*len(predictNodes)))
+
     # Run adamic adar on all nodes
     nodesCompleted = 0
     for i,node1 in enumerate(nodeRecPair):
@@ -73,10 +204,9 @@ def runAdamicAdar(G, predictNodes):
             neighborsDict[node1] = node1_neighbors
             scoreDict[node1] = {}
 
-            if nodesCompleted % 1000 == 0:
+            if nodesCompleted % 2000 == 0:
                 print("Finished %d nodes, %s" % (nodesCompleted, datetime.datetime.time(datetime.datetime.now())))
             nodesCompleted += 1
-            # scores = pool.starmap(temp, zip([node1]*len(possibleNeighbors),possibleNeighbors))
 
             # Calculate score per node
             for node2 in possibleNeighbors:
@@ -125,19 +255,20 @@ def runAdamicAdar(G, predictNodes):
             # predictions.extend([(node1, bestNode)])
 
     # Write all recommendations out
-    with open("predicted_nodes.csv", "w") as predicted:
+    with open("predicted_nodes_aa_"+dataset+".csv", "w") as predicted:
         for p in predictNodes:
             predicted.write("{}\n".format(nodeRecPair[p]))
 
     return [nodeRecPair, scoreDict]
 
-def calculateFairness(G, nodeRecPair):
+def calculateFairness(G, nodeRecPair, dataset):
     """
     Calculate Graph G fairness against Graph with adamicAdar run to generate new edges
 
     Input:
         G - networkx graph
         nodeRecPair - dict of nodes and their recommended new edge
+        dataset - string indicating what dataset is being run
 
     Output:
         Fairness score
@@ -145,7 +276,6 @@ def calculateFairness(G, nodeRecPair):
     Returns:
         float
     """
-
     G_degrees = sorted(G.degree, key=lambda x: x[1], reverse=True)
 
     # Count number of recommendations for a given node
@@ -160,6 +290,14 @@ def calculateFairness(G, nodeRecPair):
     G_aa_degrees = sorted(G_aa, key = G_aa.get, reverse=True)
     # G_aa_degrees = sorted(G_aa.degree, key=lambda x: x[1], reverse=True)
 
+    # Set up dataset specific vars
+    if dataset=='IG':
+        attr = 'gender'
+        attrVal = 'F'
+    else:
+        attr ='relativeTimestamp'
+        attrVal = 'B'
+
     # Calculate fairness score
     # Calculate number of nodes that meet percentage criteria and find % of females
     fairnessScore = 0.0
@@ -171,8 +309,8 @@ def calculateFairness(G, nodeRecPair):
         G_subset    = [node for node, degree in G_degrees[0:numOrig]]
         G_aa_subset = G_aa_degrees[0:numRec]
 
-        G_femaleCount    = sum([G.node[node]['gender']=='F' for node in G_subset])/numOrig
-        G_aa_femaleCount = sum([G.node[node]['gender']=='F' for node in G_aa_subset])/numRec
+        G_femaleCount    = sum([G.node[node][attr]==attrVal for node in G_subset])/numOrig
+        G_aa_femaleCount = sum([G.node[node][attr]==attrVal for node in G_aa_subset])/numRec
 
         fairnessScore += abs(G_femaleCount-G_aa_femaleCount)
 
@@ -181,60 +319,86 @@ def calculateFairness(G, nodeRecPair):
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser(description='')
-    parser.add_argument("--test", type=bool, default=False)
+    parser.add_argument("--test", action="store_true")
+    parser.add_argument("--customAlgo", action="store_true")
+    parser.add_argument("--SO", action="store_true")
+    parser.add_argument("--iters", type=int)
 
     args = parser.parse_args()
 
-    # inputs
-    test = args.test
+    totalFairnessCount = 0
+    totalPercentCorrect = 0
 
-    G = generateGraph()
+    for i in range(args.iters):
+        if args.SO:
+            G = generateStackOverflowGraph()
+            dataset = 'SO'
+        else:
+            G = generateInstagramGraph()
+            dataset = 'IG'
 
-    if test:
-        # Remove set of edges and test if they are added back in
-        edgeList = list(G.edges())
-        edgesRemoved = {}
-        removedEdgeList = []
-        predictNodes = []
-        for i in range(int(len(G.edges())*0.2)):
-            edge = random.choice(edgeList)
-            try:
-                G.remove_edge(*edge)
-            except nx.exception.NetworkXError:
-                continue
-            removedEdgeList.extend([edge])
-            predictNodes.extend([edge[0]])
-    else:
-        # Read in all nodes to predict
-        with open("nodes_predict.csv") as nodes:
-            nodes_predict = nodes.read().splitlines()
-        predictNodes = [int(node) for node in nodes_predict]
+        if args.test:
+            # Remove set of edges and test if they are added back in
+            edgeList = list(G.edges())
+            edgesRemoved = {}
+            removedEdgeList = []
+            predictNodes = []
+            # Num of edges to make SO and IG comparable in number of edges
+            for i in range(100000):
+                edge = random.choice(edgeList)
+                try:
+                    G.remove_edge(*edge)
+                except nx.exception.NetworkXError:
+                    continue
+                removedEdgeList.extend([edge])
+                predictNodes.extend([edge[0]])
 
-    nodeRecPair, scoreDict = runAdamicAdar(G, predictNodes)
+            # Write all nodes to recommend out
+            if dataset=='SO':
+                with open("nodes_predict_"+dataset+".csv", "w") as to_predict:
+                    for p in predictNodes:
+                        to_predict.write("{}\n".format(p))
+        else:
+            # Read in all nodes to predict
+            with open("nodes_predict.csv") as nodes:
+                nodes_predict = nodes.read().splitlines()
+            predictNodes = [int(node) for node in nodes_predict]
 
-    if test:
-        matchCount = 0.0
-        generalMatchCount = 0.0
-        valMatchCount = 0.0
-        sharedNeighborCount = 0.0
-        for source, dest in removedEdgeList:
-            if dest == nodeRecPair[source][1]:
-                matchCount += 1.0
+        if args.customAlgo:
+            nodeRecPair, scoreDict = runCustomAlgo(G, predictNodes, dataset)
+        else:
+            nodeRecPair, scoreDict = runAdamicAdar(G, predictNodes, dataset)
 
-            if dest in scoreDict[source]:
-                generalMatchCount += 1.0
-                if scoreDict[source][nodeRecPair[source][1]] == scoreDict[source][dest]:
-                    valMatchCount += 1.0
 
-            source_neighbors = list(G.neighbors(source))
-            dest_neighbors = list(G.neighbors(dest))
-            intersect = set(source_neighbors).intersection(set(dest_neighbors))
-            if intersect:
-                sharedNeighborCount += 1.0
+        if args.test:
+            matchCount = 0.0
+            generalMatchCount = 0.0
+            valMatchCount = 0.0
+            sharedNeighborCount = 0.0
+            for source, dest in removedEdgeList:
+                if dest == nodeRecPair[source][1]:
+                    matchCount += 1.0
 
-        print("Percent correct: %f" % (matchCount/len(removedEdgeList)))
-        print("Percent in neighbor set: %f" % (generalMatchCount/len(removedEdgeList)))
-        print("Percent same value: %f" % (valMatchCount/generalMatchCount))
-        print("Percent shared neighbor count: %f" % (sharedNeighborCount/len(removedEdgeList)))
-        pdb.set_trace()
-    calculateFairness(G, nodeRecPair)
+                if dest in scoreDict[source]:
+                    generalMatchCount += 1.0
+                    if scoreDict[source][nodeRecPair[source][1]] == scoreDict[source][dest]:
+                        valMatchCount += 1.0
+
+                source_neighbors = list(G.neighbors(source))
+                dest_neighbors = list(G.neighbors(dest))
+                intersect = set(source_neighbors).intersection(set(dest_neighbors))
+                if intersect:
+                    sharedNeighborCount += 1.0
+
+            print("Percent correct: %f" % (matchCount/len(removedEdgeList)))
+            print("Percent in neighbor set: %f" % (generalMatchCount/len(removedEdgeList)))
+            print("Percent same value: %f" % (valMatchCount/generalMatchCount if valMatchCount>0 else 0))
+            print("Percent shared neighbor count: %f" % (sharedNeighborCount/len(removedEdgeList)))
+            totalPercentCorrect += matchCount/len(removedEdgeList)
+        fairnessVal = calculateFairness(G, nodeRecPair, dataset)
+        totalFairnessCount += fairnessVal
+
+        print("---------------------------")
+
+    print("Average Fairness: %f" % (totalFairnessCount/args.iters))
+    print("Average Percent correct: %f" % (totalPercentCorrect/args.iters))
